bfddecf35e431b384722793a0df8c9953bbf03a6
Jones <git@jonasscheiwiller.ch>
2016-09-16 18:29:09 +0200

    [Update] Add theme_data.php TODO: Get gitignore working

---
+++ kirby/site/data/theme_data.php
@@ -0,0 +1,21 @@
+<?php
+
+class ThemeData {
+
+	function __construct() {
+		$this->kirby = kirby();
+	}
+
+	public function getGlobals() {
+		$globals = [];
+		// get only visible pages
+		$globals['menu_pages'] = $this->kirby->site()->pages()->visible();
+		return $globals;
+	}
+
+	public function getPageImages() {
+		return $this->kirby->page()->images();
+	}
+
+
+}
